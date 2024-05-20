1b8a50e68bad675d0d2de2a755a2cffaf60d1d24
Ashtosh Kumar <ashtosh16082001@gmail.com>
2024-05-20 23:57:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <div className="e-iit">
+
+      </div>
+      <div className="e-content">
+
+      </div>
+      <div className="e-subhead">
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <div className="e-button">
+        <div className="e-btn1">
+
+        </div>
+        <div className="e-btn2">
+
+        </div>
+      </div>
+      <div className="e-lasthead">
+        <div className="e-lastmini">
+          
+        </div>
+        <div className="e-lastmini">
+
+        </div>
+        <div className="e-lastmini">
+
+        </div>
+        <div className="e-lastmini">
+
+        </div>
+        <div className="e-lastmini">
+
+        </div>
+      </div>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
